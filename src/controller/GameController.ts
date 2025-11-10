419852845999c699f37b3f35abddc379ede96c74
jeong922 <hwajeong922@naver.com>
2025-11-10 14:21:17 +0900

    refactor: 초기 화면 렌더링 로직을 GameView로 분리

--- src/controller/GameController.ts
+++ src/controller/GameController.ts
@@ -1,56 +1,22 @@
+import type { IGameView } from '../view/GameView';
+
 class GameController {
   root: HTMLElement;
+  view: IGameView;
 
-  constructor(root: HTMLElement) {
+  constructor(root: HTMLElement, view: IGameView) {
     this.root = root;
+    this.view = view;
     this.init();
   }
 
   init() {
-    this.gameStart();
+    this.showStartScreen();
   }
 
-  // 게임 시작
-  gameStart() {
-    this.root.innerHTML = this.render();
+  showStartScreen() {
+    this.root.innerHTML = this.view.render();
   }
-
-  render() {
-    return `
-      <main class="game">
-        ${this.makeGameTitle()}
-        ${this.makeGameRules()}
-        ${this.makeStartButton()}
-      </main>
-    `;
-  }
-
-  makeGameRules() {
-    return `
-      <div class="game-rules">
-        <ul>
-          <li>2초 동안 보드에 표시되는 정답 위치를 기억하세요.</li>
-          <li>표시가 사라지면 제한 시간 30초 동안 정답 칸을 모두 클릭하세요.</li>
-          <li>오답 클릭 시 기회가 1 감소합니다. 총 기회는 3번 입니다.</li>
-          <li>정답 칸 중 랜덤으로 숨겨진 아이템이 있을 수 있습니다.</li>
-          <li>정답 칸을 모두 찾으면 다음 스테이지로 넘어갈 수 있습니다.</li>
-          <li>총 20 스테이지로 구성됩니다.</li>
-        </ul>
-      </div>
-    `;
-  }
-
-  makeStartButton() {
-    return `<button class="game-start">시작하기</button>`;
-  }
-
-  makeGameTitle() {
-    return `<h1>이건가...?</h1>`;
-  }
-
-  // 칸 클릭 관련 처리
-
-  // 게임 종료 결과
 }
 
 export default GameController;
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import '../styles/base.css';
 import '../styles/style.css';
 import GameController from './controller/GameController.ts';
-import Game from './model/Game.ts';
 import GameView from './view/GameView.ts';
 
 const root = document.querySelector<HTMLDivElement>('#app')! as HTMLElement;
 // const game = new Game();
-// const view = new GameView(root);
+const gameView = new GameView();
 
-new GameController(root);
+new GameController(root, gameView);
