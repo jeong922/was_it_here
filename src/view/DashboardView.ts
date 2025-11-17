4c8c0cf8d0fc0b8a2f231e0c867921132bedc86e
jeong922 <hwajeong922@naver.com>
2025-11-18 03:43:39 +0900

    feat: 남은 기회 표시 하트 아이콘으로 변경

--- src/view/DashboardView.ts
+++ src/view/DashboardView.ts
@@ -1,3 +1,4 @@
+import { setDelay } from '../utils/delay';
 import { formatTime } from '../utils/formatTime';
 import BaseView, { type IBaseView } from './BaseView';
 
@@ -14,9 +15,9 @@ class DashboardView extends BaseView implements IDashboardView {
 
   private render() {
     this.setTemplate(`
-      <div class="game-stage">스테이지 1</div>
-      <div class="game-timer">00:00</div>
-      <div class="game-lives">3회</div>
+      <div class="game-stage"></div>
+      <div class="game-timer"></div>
+      <div class="game-lives"></div>
     `);
   }
 
@@ -29,8 +30,23 @@ class DashboardView extends BaseView implements IDashboardView {
 
   setLives(lives: number): void {
     const livesElement = this.element.querySelector('.game-lives');
-    if (livesElement) {
-      livesElement.textContent = `${lives}회`;
+    if (!livesElement) return;
+
+    const currentHearts = livesElement.querySelectorAll('.lives-icon');
+    const currentCount = currentHearts.length;
+
+    if (lives < currentCount) {
+      const lostCount = currentCount - lives;
+      for (let i = 0; i < lostCount; i++) {
+        const lostHeart = currentHearts[i] as HTMLElement;
+        lostHeart.classList.add('fading-out');
+      }
+
+      setDelay(() => {
+        livesElement.innerHTML = this.renderLivesIcons(lives);
+      }, 400);
+    } else if (lives > currentCount) {
+      livesElement.innerHTML = this.renderLivesIcons(lives);
     }
   }
 
@@ -39,6 +55,16 @@ class DashboardView extends BaseView implements IDashboardView {
     if (stageElement) {
       stageElement.textContent = `스테이지 ${stage}`;
     }
+  }
+
+  private renderLivesIcons(lives: number) {
+    const heartIcon = `
+      <svg class="lives-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 576">
+        <path fill="currentColor" d="M305 151.1L320 171.8L335 151.1C360 116.5 400.2 96 442.9 96C516.4 96 576 155.6 576 229.1L576 231.7C576 343.9 436.1 474.2 363.1 529.9C350.7 539.3 335.5 544 320 544C304.5 544 289.2 539.4 276.9 529.9C203.9 474.2 64 343.9 64 231.7L64 229.1C64 155.6 123.6 96 197.1 96C239.8 96 280 116.5 305 151.1z"/>
+      </svg>
+    `;
+
+    return heartIcon.repeat(lives);
   }
 }
 
